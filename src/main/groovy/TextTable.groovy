1846b0a98a340d5489952bf0225757b54ae818db
hholmste <harnengh@gmail.com>
2013-03-20 15:46:33 +0100

    cells and rows should be first class members of this dark hierarchy

---
+++ src/main/groovy/TextTable.groovy
@@ -0,0 +1,26 @@
+class TextTable {
+
+    def symbols = [
+            internal_horizontal: '-',
+            internal_vertical: '|',
+            internal_cross: '+',
+            internal_t: '-',
+            internal_inverse_t: '-',
+            external_horizontal: '=',
+            external_vertical: '¦',
+            external_cross: '#',
+            external_t: '=',
+            external_inverse_t: '=',
+            external_upper_left: '=',
+            external_lower_left: '=',
+            external_upper_right: '=',
+            external_lower_right: '='
+    ]
+
+
+    public void printTo(OutputStream target) {
+
+    }
+
+
+}
